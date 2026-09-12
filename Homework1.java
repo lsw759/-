5795b3e175d27e8a2e7dfaa01dd224423dd927dd
lsw759 <seonwoo0819@gmail.com>
2026-09-12 17:35:41 +0900

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.printf("정수를 입력하세요: ");
+        sum =sum + sc.nextInt();
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+
+        System.out.printf("정수를 입력하세요: ");
+        sum = sum + sc.nextInt();
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+
+        System.out.printf("정수를 입력하세요: ");
+        sum = sum + sc.nextInt();
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+
+        System.out.printf("정수를 입력하세요: ");
+        sum = sum +sc.nextInt();
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+
+        System.out.printf("정수를 입력하세요: ");
+        sum = sum + sc.nextInt();
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+
+
+
+    }
+}
